c759309e6ac28d34509123905469835252b1a240
Alexander-Agafonov <agafonov.sanyas@gmail.com>
2022-09-02 23:40:02 +0100

    Added a new section

---
+++ dark.js
@@ -0,0 +1,11 @@
+function dark() {
+
+   const list = document.querySelectorAll('#nav *');
+   for (const element of list) element.classList.toggle("dark");
+   document.body.classList.toggle("dark");
+ 
+    var mode = document.querySelector("#nav img").src.replace(/^.*[\\\/]/, '');;
+    if (mode == "crescent.svg") document.querySelector("#nav img").src = "sun1.svg";
+    else document.querySelector("#nav img").src = "crescent.svg";
+ 
+ }
