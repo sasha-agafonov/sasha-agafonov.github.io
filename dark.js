fa5c60f16be03da4aa894faf1f2c8ad56cf56e8f
Alexander-Agafonov <agafonov.sanyas@gmail.com>
2022-09-04 22:58:33 +0100

    A test to see if jQuery can load HTML fragments from the server.

--- dark.js
+++ dark.js
@@ -1,11 +1,13 @@
 function dark() {
 
    const list = document.querySelectorAll('#nav *');
+
    for (const element of list) element.classList.toggle("dark");
+
    document.body.classList.toggle("dark");
- 
-    var mode = document.querySelector("#nav img").src.replace(/^.*[\\\/]/, '');;
-    if (mode == "crescent.svg") document.querySelector("#nav img").src = "sun.svg";
-    else document.querySelector("#nav img").src = "crescent.svg";
+   mode = document.querySelector("#nav img").src.replace(/^.*[\\\/]/, '');
+   
+   if (mode == "crescent.svg") document.querySelector("#nav img").src = "sun.svg";
+   else document.querySelector("#nav img").src = "crescent.svg";
  
  }
