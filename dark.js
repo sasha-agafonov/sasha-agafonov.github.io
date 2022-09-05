1c60d2dd35327e93493179ea4ccf34b72af106ff
Alexander-Agafonov <agafonov.sanyas@gmail.com>
2022-09-05 01:41:31 +0100

    Fixed navbar and HTML fragment loading

--- dark.js
+++ dark.js
@@ -6,8 +6,28 @@ function dark() {
 
    document.body.classList.toggle("dark");
    mode = document.querySelector("#nav img").src.replace(/^.*[\\\/]/, '');
+   const elem = document.getElementById("nav")
    
-   if (mode == "crescent.svg") document.querySelector("#nav img").src = "sun.svg";
-   else document.querySelector("#nav img").src = "crescent.svg";
+   //elem.style.cssText = 'background-color:red !important';
+
+   //elem.style.cssText = "--main-bg-color: red";
+
+
+  //  var r = document.querySelector(':root');
+  //  r.style.setProperty('--m2', 'green');
+
+  //  var sample = document.querySelector("#nav img");
+   //elem.style.cssText = 'background-color:red !important';
+  //  sample.setAttribute('style', 'background-color:red !important');
+   
+   if (mode == "crescent.svg") {
+    document.querySelector("#nav img").src = "sun.svg";
+    elem.style.setProperty("--main-bg-color", `white`);
+   }
+
+   else {
+    document.querySelector("#nav img").src = "crescent.svg";
+    elem.style.setProperty("--main-bg-color", `black`);
+   }
  
  }
