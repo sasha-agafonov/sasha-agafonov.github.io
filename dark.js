5a205fc847d909ac1a9bae4e6264c3eb4854155b
Alexander-Agafonov <agafonov.sanyas@gmail.com>
2022-11-09 22:13:43 +0100

    Animated hamburger icon

--- dark.js
+++ dark.js
@@ -9,18 +9,32 @@ function dark() {
    document.body.classList.toggle("dark-mode-bg");
 
    mode = document.querySelector("#nav img").src.replace(/^.*[\\\/]/, '');
+
+   // bs
+   //alert(mode);
    
    const elem = document.getElementById("nav")
    
-   if (mode == "crescent.svg") {
-    document.querySelector("#nav img").src = "sun.svg";
-    elem.style.setProperty("--main-bg-color", `white`);
-   }
+  //  if (mode == "crescent.svg") {
+  //   document.querySelector("#nav img").src = "sun.svg";
+  //   elem.style.setProperty("--main-bg-color", `white`);
+  //  }
 
-   else {
-    document.querySelector("#nav img").src = "crescent.svg";
-    elem.style.setProperty("--main-bg-color", `black`);
-   }
+  //  else {
+  //   document.querySelector("#nav img").src = "crescent.svg";
+  //   elem.style.setProperty("--main-bg-color", `black`);
+  //  }
+
+
+   switch(mode) {
+    case "crescent.svg":
+      // code block
+      break;
+
+    default:
+      document.querySelector("#nav img").src = "crescent.svg";
+      elem.style.setProperty("--main-bg-color", `black`);
+  }
  
  }
 
